3816b296f4bf9651426c603de46998b93418f7e6
Sergey <deks47@gmail.com>
2019-05-03 15:22:25 +0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,24 +3,24 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from "./components/Dialogs/Dialogs";
 
 
-const App = () => {
+const App = (props) => {
 
-  return (
-    <div className='app-wrapper'>
-    <Header />
-    <Navbar />  
-    <Profile /> 
-      
-        
-      
-      </div>);
-      
-  }
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            {/*<Profile />*/}
+            <div class='app-wrapper-content'>
+                <Dialogs />
+            </div>
 
-  
-  
+
+        </div>);
+
+}
 
 
 export default App;
