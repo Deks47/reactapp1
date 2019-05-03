6e667dab2acaa9c8fcba79ce978a7b672318a233
Sergey <deks47@gmail.com>
2019-05-03 15:22:36 +0300

    Update Profile.jsx

--- my-app/src/components/Profile/Profile.jsx
+++ my-app/src/components/Profile/Profile.jsx
@@ -3,19 +3,17 @@ import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
-  return <div className={s.content}>
-      
-      <div>
-        <img src='http://www.fotoprizer.ru/img/120296_orig.jpg'/>
-      </div>
-      
-      <div>ava+description</div>
-      <MyPosts />
-      
-      
-    
-    </div>
-    
+    return (
+        <div className={s.content}>
+            <div>
+                <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350' />
+            </div>
+            <div>
+                ava + description
+            </div>
+            <MyPosts />
+        </div>
+    )
 }
 
 export default Profile;
