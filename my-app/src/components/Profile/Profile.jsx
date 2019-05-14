d476b4280bb8e33de616b7a621e28775cc0a8700
Deks47 <deks47@gmail.com>
2019-05-14 15:50:06 +0300

    Update Profile.jsx

--- my-app/src/components/Profile/Profile.jsx
+++ my-app/src/components/Profile/Profile.jsx
@@ -4,8 +4,10 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
     return (
-        <div className={s.content}>
-            <div>
+        <div>
+            <ProfileInfo />
+        </div>
+
                 <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350' />
             </div>
             <div>
