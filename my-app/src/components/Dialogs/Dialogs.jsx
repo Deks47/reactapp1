4b0b1e48ec91ece8bae4ac5b1aa0b888e99a17aa
Deks47 <deks47@gmail.com>
2019-05-21 17:14:42 +0300

    Update Dialogs.jsx

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -15,24 +15,36 @@ const Message = (props) => {
 
 const Dialogs = (props) => {
 
+let dialogsData = [
+    {id: 1, name: 'John'},
+    {id: 2, name: 'James'},
+    {id: 3, name: 'Kate'},
+    {id: 4, name: 'Said'},
+    {id: 5, name: 'Hurley'},
+    {id: 6, name: 'Jack'},
+]
+
+    let messagesData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'Allow me introduce myself'},
+
+    ]
 
     return (
         <div className="{s.dialogs}">
             <div className="{s.dialogsItems}">
 
-                <DialogItem name="John" id="1"/>
-                <DialogItem name="James" id="2"/>
-                <DialogItem name="Kate" id="3"/>
-                <DialogItem name="Said" id="4"/>
-                <DialogItem name="Hurley" id="5"/>
-                <DialogItem name="Jack" id="6"/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+
 
 
             </div>
             <div className="{s.messages}">
-                <Message message="Hi" />
-                <Message message="How are you?" />
-                <Message message="Allow me introduce myself" />
+                <Message message={messagesData[0].message} />
+                <Message message={messagesData[1].message} />
+
 
 
 
