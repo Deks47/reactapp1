ea1852e9901d2620d497eb0070faec7a87a40487
Deks47 <deks47@gmail.com>
2019-05-22 17:27:59 +0300

    Update Dialogs.jsx

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -15,37 +15,38 @@ const Message = (props) => {
 
 const Dialogs = (props) => {
 
-let dialogsData = [
-    {id: 1, name: 'John'},
-    {id: 2, name: 'James'},
-    {id: 3, name: 'Kate'},
-    {id: 4, name: 'Said'},
-    {id: 5, name: 'Hurley'},
-    {id: 6, name: 'Jack'},
-]
+    let dialogs = [
+        {id: 1, name: 'John'},
+        {id: 2, name: 'James'},
+        {id: 3, name: 'Kate'},
+        {id: 4, name: 'Said'},
+        {id: 5, name: 'Hurley'},
+        {id: 6, name: 'Jack'},
+    ]
 
-    let messagesData = [
+    let messages = [
         {id: 1, message: 'Hi'},
         {id: 2, message: 'How are you?'},
         {id: 3, message: 'Allow me introduce myself'},
 
     ]
 
+
+    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+
+    let messagesElements = messages.map(m => <Message message={m.message}/>);
+
+
     return (
         <div className="{s.dialogs}">
             <div className="{s.dialogsItems}">
 
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-
+                {dialogsElements}
 
 
             </div>
             <div className="{s.messages}">
-                <Message message={messagesData[0].message} />
-                <Message message={messagesData[1].message} />
-
-
+                {messagesElements}
 
 
             </div>
