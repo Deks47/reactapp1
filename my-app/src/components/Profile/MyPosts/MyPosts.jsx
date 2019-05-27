3b663636cd0ec4b5b6e136dc570dd0f049f2e699
Deks47 <deks47@gmail.com>
2019-05-27 16:06:11 +0300

    Update MyPosts.jsx

--- my-app/src/components/Profile/MyPosts/MyPosts.jsx
+++ my-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,16 +2,10 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
-const MyPosts = () => {
-    let posts = [
-        {id: 1, message: 'Test drive', likesCount: 0},
-        {id: 2, message: 'First post', likesCount: 23},
-
-
-    ]
-
+const MyPosts = (props) => {
     let postsElements =
-        posts.map( p => <Post message={p.message} likesCount={p.likesCount} /> );
+        props.posts.map( p => <Post message={p.message} likesCount={p.likesCount} /> );
+
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
