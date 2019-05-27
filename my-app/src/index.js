25bac8e718f7196b689dbfd13b4a262176f269a8
Deks47 <deks47@gmail.com>
2019-05-27 16:06:15 +0300

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,7 +4,29 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let posts = [
+    {id: 1, message: 'Test drive', likesCount: 0},
+    {id: 2, message: 'First post', likesCount: 23},
+
+
+]
+
+let dialogs = [
+    {id: 1, name: 'John'},
+    {id: 2, name: 'James'},
+    {id: 3, name: 'Kate'},
+    {id: 4, name: 'Said'},
+    {id: 5, name: 'Hurley'},
+    {id: 6, name: 'Jack'},
+]
+
+let messages = [
+    {id: 1, message: 'Hi'},
+    {id: 2, message: 'How are you?'},
+    {id: 3, message: 'Allow me introduce myself'},
+
+]
+ReactDOM.render(<App posts={posts} dialogs={dialogs} messages={messages} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
