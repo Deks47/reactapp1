74225bf4f3272ed7c2aedc837895cceef086d443
Deks47 <deks47@gmail.com>
2019-05-27 16:06:08 +0300

    Update Dialogs.jsx

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -6,26 +6,12 @@ import Message from "./Message/Message";
 
 const Dialogs = (props) => {
 
-    let dialogs = [
-        {id: 1, name: 'John'},
-        {id: 2, name: 'James'},
-        {id: 3, name: 'Kate'},
-        {id: 4, name: 'Said'},
-        {id: 5, name: 'Hurley'},
-        {id: 6, name: 'Jack'},
-    ]
-
-    let messages = [
-        {id: 1, message: 'Hi'},
-        {id: 2, message: 'How are you?'},
-        {id: 3, message: 'Allow me introduce myself'},
-
-    ]
 
 
-    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
 
-    let messagesElements = messages.map(m => <Message message={m.message}/>);
+    let dialogsElements = props.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+
+    let messagesElements = props.messages.map(m => <Message message={m.message}/>);
 
 
     return (
