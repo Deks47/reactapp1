d46fe4f14050bfec7b65f88d40c176024b829940
Deks47 <deks47@gmail.com>
2019-05-28 16:49:18 +0300

    Create state.js

---
+++ my-app/src/redux/state.js
@@ -0,0 +1,31 @@
+let state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: 'Test drive', likesCount: 0},
+            {id: 2, message: 'First post', likesCount: 23},
+
+
+        ],
+
+    },
+
+    dialogsPage: {
+        dialogs: [
+            {id: 1, name: 'John'},
+            {id: 2, name: 'James'},
+            {id: 3, name: 'Kate'},
+            {id: 4, name: 'Said'},
+            {id: 5, name: 'Hurley'},
+            {id: 6, name: 'Jack'},
+        ],
+    messages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'Allow me introduce myself'},
+
+    ]
+}
+
+}
+
+export default state;
