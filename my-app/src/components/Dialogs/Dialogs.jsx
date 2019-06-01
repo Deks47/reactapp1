720ac27a682da7d2e7e69d6554fd63402a430884
Sergey <deks47@gmail.com>
2019-06-01 19:10:18 +0300

    Update Dialogs.jsx

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,35 +1,29 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
 
 const Dialogs = (props) => {
 
+
+
+
+    let dialogsElements =  props.state.dialogs.map( d => <DialogItem name={d.name} id={d.id} />  );
+    let messagesElements = props.state.messages.map( m => <Message message={m.message}/> );
+
+
     return (
         <div className="{s.dialogs}">
             <div className="{s.dialogsItems}">
-                <div className="{s.dialog + ' ' + s.active}">
-                    John
-                    </div>
-                <div className="{s.dialog}">
-                    James
-                </div>
-                <div className="{s.dialog}">
-                    Kate
-                </div>
-                <div className="{s.dialog}">
-                    Said
-                </div>
-                <div className="{s.dialog}">
-                    Hurley
-                </div>
-                <div className="{s.dialog}">
-                    Jack
-                </div>
+
+                {dialogsElements}
+
+
             </div>
             <div className="{s.messages}">
-                <div className="{s.dialog}">Hi</div>
-                <div className="{s.dialog}">How are you?</div>
-                <div className="{s.dialog}">Allow me introduce myself</div>
+                {messagesElements}
+
 
             </div>
         </div>
