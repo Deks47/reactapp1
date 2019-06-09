d76c9891cc4fc95449e839cf468d81c612e292dc
Sergey <deks47@gmail.com>
2019-06-09 19:20:46 +0300

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,18 @@
+import * as serviceWorker from './serviceWorker';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import state from './redux/state.js';
+import state from "./redux/state";
+import {addPost} from "./redux/state";
+import { BrowserRouter } from "react-router-dom";
+
 
 
 ReactDOM.render(
 
-    <App state={state} />, document.getElementById('root'));
+    <App state={state} addPost={addPost} />,
+    document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
