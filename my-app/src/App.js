fc7742e318e1e865f1659cd025105ed6034e65ff
Deks47 <deks47@gmail.com>
2019-06-10 17:48:55 +0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,14 +4,14 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 
 
 const App = (props) => {
 
 
     return (
-        <BrowserRouter>
+
             <div className='app-wrapper'>
                 <Header/>
                 <Navbar/>
@@ -19,20 +19,21 @@ const App = (props) => {
                 <div className='app-wrapper-content'>
 
 
-                    <Route path='/profile'
-                           render={() => <Profile
-                               state={props.state.profilePage}
-                               addPost={props.addPost}/> }/>
                     <Route path='/dialogs'
-                           render={() => <Dialogs
-                               state={props.state.dialogsPage} /> } />
+                           render={ () => <Dialogs state={props.state.dialogsPage} /> }/>
+
+                    <Route path='/profile'
+
+                    render={ () => <Profile
+                    state={props.state.profilePage}
+                    addPost={props.addPost} /> }/>
 
 
 
                 </div>
             </div>
 
-        </BrowserRouter>)
+    )
 
 }
 
