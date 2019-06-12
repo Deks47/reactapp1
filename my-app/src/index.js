becb4ee33f1be7ae3319015253be07e3f1fe0d44
Sergey <deks47@gmail.com>
2019-06-12 21:54:17 +0300

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,16 +1,16 @@
 import * as serviceWorker from './serviceWorker';
-import {rerenderEntireTree} from "./render";
 import state from "./redux/state";
+import {rerenderEntireTree} from "./render";
 
 rerenderEntireTree(state);
 
-<<<<<<< HEAD
-.....
-ReactDOM.render(
-=======
-
->>>>>>> adcc0edf521aa218cbc33e7bcd764647cf66b432
-
-
 
 serviceWorker.unregister();
+
+
+
+
+
+
+
+
