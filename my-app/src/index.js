5043332a3a6a5bff3abe730cbc138653419e7858
Sergey <deks47@gmail.com>
2019-06-12 22:32:32 +0300

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,23 @@
 import * as serviceWorker from './serviceWorker';
-import state from "./redux/state";
-import {rerenderEntireTree} from "./render";
+import state, {subscribe} from "./redux/state";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {addPost, updateNewPostText} from './redux/state';
+import {BrowserRouter} from "react-router-dom";
 
+
+let rerenderEntireTree = () => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+        </BrowserRouter>, document.getElementById('root'));
+}
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
+
 
 
 serviceWorker.unregister();
